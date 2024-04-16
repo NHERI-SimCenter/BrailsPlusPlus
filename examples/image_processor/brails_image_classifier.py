bebddda4b9a77114da66a3a129bff0a745312d69
fmckenna <fmckenna@berkeley.edu>
2024-04-16 09:51:01 -0700

    fmk - updating image classifier example with clear, at least i think, roof type images

--- examples/image_processor/brails_image_classifier.py
+++ examples/image_processor/brails_image_classifier.py
@@ -1,62 +1,35 @@
-# -*- coding: utf-8 -*-
-#
-# Copyright (c) 2024 The Regents of the University of California
-#
-# This file is part of BRAILS++.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-# 1. Redistributions of source code must retain the above copyright notice,
-# this list of conditions and the following disclaimer.
-#
-# 2. Redistributions in binary form must reproduce the above copyright notice,
-# this list of conditions and the following disclaimer in the documentation
-# and/or other materials provided with the distribution.
-#
-# 3. Neither the name of the copyright holder nor the names of its contributors
-# may be used to endorse or promote products derived from this software without
-# specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# You should have received a copy of the BSD 3-Clause License along with
-# BRAILS. If not, see <http://www.opensource.org/licenses/>.
-#
-# Contributors:
-# Frank McKenna
-# Barbaros Cetiner
-#
-# Last updated:
-# 04-11-2024  
-#
-# Purpose: Testing ImageSet, Importer and an aerial image classifier
+'''
+Purpose: Testing ImageSet, Importer and an aerial image classifier
+'''
 
-from brails import Importer
-from brails.types.image_set import ImageSet
+# Written: fmk 04/24
+# Copyright BSD2
+
+import sys
+import importlib.util
+
+#if importlib.util.find_spec("brails") is None:
+sys.path.insert(1,'../../')
+
+from brails.types.image_set import ImageSet    
+from brails.utils.utils import Importer
 
 importer = Importer()
 aerial_images = ImageSet();
 street_images = ImageSet();
-aerial_images.set_directory("./images/satellite", True)
+aerial_images.set_directory("./images/satellite_easy", True)
 street_images.set_directory("./images/street", True)
 
+aerial_images.print()
+
 # Test importer and one of the aerial imagery classifiers, RoofShapeClassifier:
+print('ROOF_SHAPE_CLASSICAL PREDICTIONS')
 my_class = importer.get_class('RoofShapeClassifier')
 my_classifier = my_class()
 predictions = my_classifier.predict(aerial_images)
 print(predictions)
 
+print('ROOF_SHAPE_LLM PREDICTIONS')
 my_class = importer.get_class('RoofShapeLLM')
 my_classifier = my_class()
 predictions = my_classifier.predict(aerial_images)
@@ -64,6 +37,7 @@ print(predictions)
 
 # Test importer and one of the street-level imagery classifiers, 
 # OccupancyClassifier:
+print('OCCUPANCY_CLASS_CLASSICAL PREDICTIONS')
 my_class = importer.get_class('OccupancyClassifier')
 my_classifier = my_class()
 predictions = my_classifier.predict(street_images) 
