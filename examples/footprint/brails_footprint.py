fac16b7949fecb44e46dde7523a58709ec1d72bb
fmckenna <fmckenna@berkeley.edu>
2024-04-17 15:55:11 -0700

    fmk  -updating footprint example

--- examples/footprint/brails_footprint.py
+++ examples/footprint/brails_footprint.py
@@ -1,67 +1,41 @@
-# -*- coding: utf-8 -*-
-#
-# Copyright (c) 2024 The Regents of the University of California
-#
-# This file is part of BRAILS++.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-# 1. Redistributions of source code must retain the above copyright notice,
-# this list of conditions and the following disclaimer.
-#
-# 2. Redistributions in binary form must reproduce the above copyright notice,
-# this list of conditions and the following disclaimer in the documentation
-# and/or other materials provided with the distribution.
-#
-# 3. Neither the name of the copyright holder nor the names of its contributors
-# may be used to endorse or promote products derived from this software without
-# specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# You should have received a copy of the BSD 3-Clause License along with
-# BRAILS. If not, see <http://www.opensource.org/licenses/>.
-#
-# Contributors:
-# Frank McKenna
-# Barbaros Cetiner
-#
-# Last updated:
-# 04-11-2024  
-#
-# Purpose: Testing Importer and get_footprints methods
+# Written: fmk 4/23
+# License: BSD-2
 
-from brails import Importer
+"""
+ Purpose: Testing Importer and get_footprints methods
+"""
+
+import sys
+
+sys.path.insert(1, "../../")
+
+from brails.utils.utils import Importer
+
+
 
 importer = Importer()
 
-region_data = {
-    "type":"locationName",
-    "data":"Tiburon, CA"
-}
+region_data = {"type": "locationName", "data": "Tiburon, CA"}
 
-region_boundary_class = importer.get_class('RegionBoundary')
-region_boundary_object = region_boundary_class(region_data);
+region_boundary_class = importer.get_class("RegionBoundary")
+region_boundary_object = region_boundary_class(region_data)
 
-print('Trying OSM_FootprintsScraper')
-osm_class = importer.get_class('OSM_FootprintScraper')
-osm_data = {'length':'ft'}
+print("Trying OSM_FootprintsScraper")
+osm_class = importer.get_class("OSM_FootprintScraper")
+osm_data = {"length": "ft"}
 instance1 = osm_class(osm_data)
-instance1.get_footprints(region_boundary_object)
+osm_inventory = instance1.get_footprints(region_boundary_object)
 
-print('Trying USA_FootprintsScraper')
-usa_class = importer.get_class('OSM_FootprintScraper')
-usa_data = {'length':'ft'}
+print("num assets OSM", len(osm_inventory.inventory))
+
+print("Trying USA_FootprintsScraper")
+usa_class = importer.get_class("USA_FootprintScraper")
+usa_data = {"length": "ft"}
 instance2 = usa_class(usa_data)
-instance2.get_footprints(region_boundary_object)
+usa_inventory = instance2.get_footprints(region_boundary_object)
+
+print("num assets USA", len(usa_inventory.inventory))
+
+# just get images for a small subset of the assets
+small_inventory = usa_inventory.get_random_sample(4, 200)
+print("num assets USA seubset", len(small_inventory.inventory))
