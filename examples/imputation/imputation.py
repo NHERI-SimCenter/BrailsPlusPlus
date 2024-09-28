4bd2fc934f7adc3fb9a2ab39d3deb56ae452e3ef
fmckenna <fmckenna@berkeley.edu>
2024-09-27 21:35:42 -0700

    fmk - initial doc building

---
+++ examples/imputation/imputation.py
@@ -0,0 +1,32 @@
+"""
+imputation.py
+================
+
+This is a simple BRAILS example to demonstrate imputating (estimating the
+missing pieces) of an inventory dataset.
+
+"""
+
+import os
+import sys
+
+# the following line is not neeeded if brails is imported from pypi
+#   .. it is included here as it allows us to test the code on a nightly basis
+sys.path.insert(1, "../../")
+
+from brails.utils.utils import Importer
+from brails.types.image_set import ImageSet
+from brails.types.asset_inventory import Asset, AssetInventory
+importer = Importer()
+
+file_path = "./example_Tiburon.csv"
+    
+inventory = AssetInventory()
+inventory.read_from_csv(file_path,keep_existing=True, id_column='index')
+
+
+knn_imputer_class = importer.get_class("KnnImputer")
+imputer=knn_imputer_class()
+new_inventory = imputer.impute(inventory,n_possible_worlds=10)
+
+new_inventory.print()
