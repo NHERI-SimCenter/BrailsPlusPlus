da6f9c8c54e465e0f2c304c94fc46e4e923665c3
bacetiner <bacetiner@ucla.edu>
2024-10-11 13:43:44 -0700

    bc - Cleaned CLIPClassifier and fixed model_path inconsistencies

--- brails/processors/vlm_image_classifier/CLIPClassifier.py
+++ brails/processors/vlm_image_classifier/CLIPClassifier.py
@@ -1,8 +1,8 @@
-# -*- coding: utf-8 -*-
+"""Class object to call CLIP for image classification."""
 #
-# Copyright (c) 2022 The Regents of the University of California
+# Copyright (c) 2024 The Regents of the University of California
 #
-# This file is part of BRAILS.
+# This file is part of BRAILS++.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
@@ -35,97 +35,171 @@
 #
 # Contributors:
 # Brian Wang
+# Barbaros Cetiner
+#
+# Last updated:
+# 10-11-2024
 
 
 import torch
-import torch.nn as nn
-import numpy as np
-import matplotlib.pyplot as plt
-import time
 import os
-import copy
 from PIL import Image
-import sys
-import zipfile
-import json
 from .clip.clip import load, tokenize
-from .clip.model import build_model
 from .clip.utils import compute_similarity, aggregate_predictions
 from brails.types.image_set import ImageSet
-from typing import Optional, Dict
+from typing import Optional, List, Dict
 
 
-#import utils
 class CLIPClassifier:
-    def __init__(self, task, input_dict: Optional[dict] =None):
-        #initializr model, predict, retrain() -> don't support train() (from scratch) for now
-        if(input_dict != None):
-            self.modelArch = input_dict['model_arch']
-        else:
-            self.modelArch = "ViT-B/32"
+    """
+    A classifier that utilizes CLIP model to predict attributes from images.
 
-        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")   
+    This class is designed to load a CLIP model, process input images,
+    and make predictions for the entered textual prompts. It supports
+    customizable classes and prompts to enhance prediction accuracy.
+
+    Attributes__
+        model_arch (str): The architecture of the model to be used. Available
+            model architectures are 'ViT-B/32' (default), RN50', 'RN101',
+            'RN50x4','RN50x16', 'RN50x64', 'ViT-B/16', 'ViT-L/14', and
+            'ViT-L/14@336px'.
+        device (torch.device): The device (CPU or GPU) used for computations.
+        preds (Optional[Dict[str, str]]): A dictionary to hold predictions.
+        batch_size (int): The number of images processed in a single batch.
+        template (str): A template for formatting text prompts.
+
+    Args__
+        task (str): The task for which the classifier is being used.
+        input_dict (Optional[dict]): A dictionary containing model architecture
+            and other configuration parameters.
+    """
+
+    def __init__(self, task: str, input_dict: Optional[dict] = None):
+        """
+        Initialize CLIPClassifier.
+
+        Args__
+            task (str): The task for which the classifier is being used.
+            input_dict (Optional[dict]): A dictionary containing model
+                architecture and other configuration parameters.
+        """
+        if (input_dict is not None):
+            self.model_arch = input_dict['model_arch']
+        else:
+            self.model_arch = "ViT-B/32"
+
+        # Set default model path based on model architecture:
+        self.model_path = f'tmp/models/{self.model_arch.replace("/", "-")}.pth'
+
+        self.device = torch.device(
+            "cuda:0" if torch.cuda.is_available() else "cpu")
         self.preds = None
         self.batch_size = 20
-        self.template = "a photo of a {}" 
+        self.template = "a photo of a {}"
 
-    def predict(self, images: ImageSet, modelPath='tmp/models/VIT-B-32.pth', 
-                classes=None, text_prompts = None):
-        assert classes==None or (classes!=None and text_prompts!=None), 'customized classes provide customized prompts (text_prompts can not be None)'
-        if(classes != None):
-            assert len(text_prompts) % len(classes) == 0, "number of text prompts should be equal across classes (i.e. number of text prompts should be multiples of num classes)"
-        if(classes!=None):
+    def predict(self,
+                images: ImageSet,
+                model_path: str = '',
+                classes: Optional[List[str]] = None,
+                text_prompts: Optional[List[str]] = None
+                ) -> Dict[str, str]:
+        """
+        Predicts classes for the given images using the CLIP model.
+
+        Args__
+            images (ImageSet): An object containing the images to classify.
+            model_path (str): The path to the pre-trained model.
+            classes (Optional[List[str]]): A list of class names.
+            text_prompts (Optional[List[str]]): A list of text prompts
+                corresponding to the classes.
+
+        Returns__
+            Dict[str, str]: A dictionary mapping image keys to their predicted
+                classes.
+
+        Raises__
+            AssertionError: If the conditions on classes and text_prompts are
+                not met.
+        """
+        assert (
+            classes is None or
+            (classes is not None and text_prompts is None)
+        ), 'Customized classes provide customized prompts (text_prompts cannot'
+        ' be None)'
+        if (classes is not None):
+            assert (
+                len(text_prompts) % len(classes) == 0
+            ), 'Number of text prompts should be equal across classes (i.e.,'
+            ' the number of text prompts should be a multiple of the number'
+            ' of classes)'
+
+        if (classes is not None):
             self.classes, self.text_prompts = classes, text_prompts
-        
-        self.modelPath = modelPath
-        
-        def isImage(im):
-            return im.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp'))
-        
-        #download CLIP model & load to device
-        if(os.path.exists(modelPath)):
-            model, data_transforms = load(modelPath, self.device, modelPath) #load model
+
+        if model_path:
+            self.model_path = model_path
         else:
-            download_root = os.path.dirname(modelPath)
-            os.makedirs(download_root, exist_ok = True)
-            model, data_transforms = load(self.modelArch, self.device, download_root = download_root) #download model if not found
+            model_path = self.model_path
+
+        def isImage(im: str) -> bool:
+            return im.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp'))
+
+        # Download CLIP model & load to device:
+        if (os.path.exists(model_path)):
+            model, data_transforms = load(
+                model_path, self.device, model_path)  # load model
+        else:
+            download_root = os.path.dirname(model_path)
+            os.makedirs(download_root, exist_ok=True)
+            # Download model if not found:
+            model, data_transforms = load(
+                self.model_arch, self.device, download_root=download_root)
         model.eval()
 
-        #tokenize text prompts
-        text_input = torch.cat([tokenize(self.template.format(c)) for c in self.text_prompts]).to(self.device)
+        # Tokenize text prompts:
+        text_input = torch.cat([tokenize(self.template.format(c))
+                               for c in self.text_prompts]).to(self.device)
         prompts_per_class = len(self.text_prompts) // len(self.classes)
-        
+
         preds = {}
         batch_imgs = []
         batch_keys = []
         data_dir = images.dir_path
         if not os.path.isdir(data_dir):
-            print('ImageClassifier fails as ', data_dir, ' is not a directory')
+            print('ImageClassifier failed as ', data_dir, ' is not a valid '
+                  'directory')
 
         for idx, (key, im) in enumerate(images.images.items()):
             if isImage(im.filename):
-                image = Image.open(os.path.join(data_dir, im.filename)).convert("RGB")
+                image = Image.open(os.path.join(
+                    data_dir, im.filename)).convert("RGB")
                 image = data_transforms(image).float()
                 image = image.to(self.device)
                 image = image.unsqueeze(0)
                 batch_imgs.append(image)
-                batch_keys.append(key)  
+                batch_keys.append(key)
 
-            #batch processing (batch size = 20)
-            if((idx != 0 and idx % self.batch_size == 0) or idx == len(images.images)-1):
+            # Batch processing (batch size = 20):
+            if ((idx != 0 and idx % self.batch_size == 0) or
+                    idx == len(images.images)-1):
                 image_input = torch.cat(batch_imgs)
                 with torch.no_grad():
                     image_features = model.encode_image(image_input)
                     text_features = model.encode_text(text_input)
-                #For k prompts/class, C classes, N images, compute matrix of dimension N x (kC)
+                # Compute similarity matrix. For k prompts/class, C classes,
+                # N images, compute matrix of dimension N x (kC):
                 similarity = compute_similarity(image_features, text_features)
-                #aggregate matrix of Nxkc into N x C matrix, then select the label with largest score as prediction
-                batch_preds, _ = aggregate_predictions(similarity, agg_method = "max", gap = prompts_per_class)
+
+                # Aggregate matrix of Nxkc into N x C matrix, then select the
+                # label with largest score as prediction:
+                batch_preds, _ = aggregate_predictions(
+                    similarity, agg_method="max", gap=prompts_per_class)
                 for (k, p) in zip(batch_keys, batch_preds):
-                    preds[k]=self.classes[p.item()]
-                
+                    preds[k] = self.classes[p.item()]
+
                 batch_imgs, batch_keys = [], []
         return preds
+
 
 if __name__ == '__main__':
     pass
