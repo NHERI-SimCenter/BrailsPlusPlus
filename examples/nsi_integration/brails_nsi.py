aca411bfeca3cb5484633a41a65b2ddd6b9a3ef0
bacetiner <bacetiner@ucla.edu>
2024-11-06 21:51:22 -0800

    bc - Added extra commentary to the example

--- examples/nsi_integration/brails_nsi.py
+++ examples/nsi_integration/brails_nsi.py
@@ -1,69 +1,107 @@
-# Written: fmk 4/23
-# License: BSD-2
+# Copyright (c) 2024 The Regents of the University of California
+#
+# This file is part of BRAILS++.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its contributors
+# may be used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS'
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# You should have received a copy of the BSD 3-Clause License along with
+# BRAILS++. If not, see <http://www.opensource.org/licenses/>.
+#
+# Contributors:
+# Frank McKenna
+# Barbaros Cetiner
+#
+# Last updated:
+# 11-06-2024
 
 """
- Purpose: Testing Importer and get_footprints methods
+Example demonstrating the use of NSI scraper module .
+
+ Purpose: Testing 1) get_class method of Importer
+                  2) get_raw_data_given_boundary and
+                     get_filtered_data_given_inventory methods of NSI_Parser
+                  3) get_footprints method of OSM_FootprintScraper module
+                  4) print_info and write_to_geojson method of AssetInventory
+                     objects
 """
 
 import os
-import copy
 import json
-
 from brails import Importer
 
-# create an Import to get the classes
-
+# Create an importer to get the required classes:
 importer = Importer()
 
-#
-# specify the BoundaryRegion & then use an NSI_Parser to get geojson
-#
-
-
-region_data = {"type": "locationName", "data": "Tiburon, CA"}
-region_boundary_class = importer.get_class("RegionBoundary")
+# Select a region and create its RegionBoundary:
+region_data = {'type': 'locationName', 'data': 'Tiburon, CA'}
+region_boundary_class = importer.get_class('RegionBoundary')
 region_boundary_object = region_boundary_class(region_data)
 
-nsi_class = importer.get_class("NSI_Parser")
+# Use NSI_Parser to get the NSI raw data for the specified region:
+nsi_class = importer.get_class('NSI_Parser')
 nsi = nsi_class()
 inventory = nsi.get_raw_data_given_boundary(region_boundary_object, 'ft')
 inventory.print_info()
 
+# Get the footprints for the defined region from OSM:
+print('Trying OSM_FootprintsScraper...')
 
-#
-# Get Footprints using OSM
-#
-
-print("Trying OSM_FootprintsScraper ...")
-
-osm_class = importer.get_class("OSM_FootprintScraper")
-osm_data = {"length": "ft"}
+osm_class = importer.get_class('OSM_FootprintScraper')
+osm_data = {'length': 'ft'}
 osm = osm_class(osm_data)
 osm_inventory = osm.get_footprints(region_boundary_object)
+print('Total number of assets detected using OSM:',
+      len(osm_inventory.inventory))
 
-small_inventory = osm_inventory.get_random_sample(3, 200)
-copy_inventory = copy.deepcopy(small_inventory)
+# Get a small subset of the obtained footprints:
+osm_inventory_subset = osm_inventory.get_random_sample(5, 200)
+print('\nTotal number of assets in the randomly selected subset:',
+      len(osm_inventory_subset.inventory))
 
 print('\n******** SMALL INVENTORY *********')
-small_inventory.print_info()
+osm_inventory_subset.print_info()
 
+# Get the NSI raw data for this subset:
 print('\n******** NSI RAW DATA FOR SMALL INVENTORY *********')
-small_inventory = nsi.get_raw_data_given_inventory(small_inventory, 'ft')
-small_inventory.print_info()
+subset_nsi_raw_data = nsi.get_raw_data_given_inventory(
+    osm_inventory_subset, 'ft')
+subset_nsi_raw_data.print_info()
 
+# Get the NSI filtered data for this subset:
 print('\n******** NSI FILTERED DATA FOR SMALL INVENTORY *********')
-# another_small_inventory = osm_inventory.get_random_sample(4, 200)
-copy_inventory = nsi.get_filtered_data_given_inventory(copy_inventory, 'ft')
-copy_inventory.print_info()
+subset_nsi_processed_data = nsi.get_filtered_data_given_inventory(
+    osm_inventory_subset, 'ft')
+subset_nsi_processed_data.print_info()
 
-geojson = copy_inventory.get_geojson()
+# Write the extracted inventory to a GeoJSON file:
+FILE_PATH = 'tmp/smallinv.geojson'
+directory = os.path.dirname(FILE_PATH)
+os.makedirs(directory, exist_ok=True)
 
-# pretty_geojson = json.dumps(geojson, indent=2)
-# print(pretty_geojson)
-
-filepath = 'tmp/smallinv.geojson'
-directory = os.path.dirname(filepath)
-if not os.path.exists(directory):
-    os.makedirs(directory)
-with open(filepath, "w") as f:
+geojson = subset_nsi_processed_data.write_to_geojson()
+with open(FILE_PATH, 'w', encoding='utf8') as f:
     json.dump(geojson, f, indent=2)
