ec6e77245e6c2a7be9b17f4c1a14e85d42971a21
bacetiner <bacetiner@ucla.edu>
2025-06-03 14:51:18 -0700

    bc - Updating example with the spatial join for points in or near polygons

--- examples/spatial_joins/brails_join_methods.py
+++ examples/spatial_joins/brails_join_methods.py
@@ -10,7 +10,7 @@ on AssetInventory objects
 """
 
 from brails.utils import Importer
-from brails.utils import SpatialJoinMethods
+from copy import deepcopy
 
 # Define inventory location:
 LOCATION = "Tiburon, CA"
@@ -33,7 +33,14 @@ fp_scraper_class = importer.get_class('OSM_FootprintScraper')
 fp_scraper = fp_scraper_class({'length': 'ft'})
 fp_inventory = fp_scraper.get_footprints(region_boundary_object)
 
-# Merge NSI data with FEMA USA structures data:
-merged_inventory = SpatialJoinMethods.execute('GetPointsInPolygons',
-                                              fp_inventory,
-                                              nsi_inventory)
+# Merge NSI data with FEMA USA Structures data using a spatial join
+# that identifies points within polygons:
+merged_inventory1 = deepcopy(fp_inventory)
+merged_inventory1.join(inventory_to_join=nsi_inventory,
+                       method='GetPointsInPolygons')
+
+# Merge NSI data with FEMA USA Structures data using a spatial join
+# that identifies points within or nearest to polygons:
+merged_inventory2 = deepcopy(fp_inventory)
+merged_inventory2.join(inventory_to_join=nsi_inventory,
+                       method='GetPointsNearPolygons')
