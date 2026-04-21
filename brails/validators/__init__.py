ffe50e9683c0d699b8a51a06a59949a34526b684
fmckenna <fmckenna@berkeley.edu>
2026-04-21 14:26:13 -0700

    fmk - fixing init.py in validators

--- brails/validators/__init__.py
+++ brails/validators/__init__.py
@@ -1,5 +1,4 @@
 from .inventory_validator import InventoryValidator
-from .hazusEarthquake.hazus_earthquake_validator import HazusEarthquakeValidator
 
-__all__ = ["InventoryValidator","HazusEarthquakeValidator"]
+__all__ = ["InventoryValidator"]
 
